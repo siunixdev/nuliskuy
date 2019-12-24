ceb9257ada25481c884fa69373de408c54d7c1a3
Siunix <abd.siunix@gmail.com>
2019-12-24 11:38:11 +0700

    Update category

--- controller/category.js
+++ controller/category.js
@@ -7,21 +7,26 @@ const follow = model.follow;
 
 // GET LIST
 exports.list = (req, res) => {
+  let message = "";
+
   Category.findAll({
     attributes: {
       exclude: ["is_published", "is_archived", "createdAt", "updatedAt"]
     }
   })
-    .then(category => {
-      res.status(200).json(category);
+    .then(data => {
+      res.status(200).json(data);
     })
     .catch(error => {
-      res.status(400).json({ message: "Bad request" });
+      message = "Server response error";
+      res.status(500).json({ message });
     });
 };
 
 // GET DETAIL
 exports.detail = (req, res) => {
+  let message = "";
+
   Category.findOne({
     attributes: {
       exclude: ["is_published", "is_archived", "createdAt", "updatedAt"]
@@ -30,41 +35,82 @@ exports.detail = (req, res) => {
       id: req.params.id
     }
   })
-    .then(category => {
-      res.status(200).json(category);
+    .then(data => {
+      res.status(200).json(data);
     })
     .catch(error => {
-      res.status(400).json({ message: "Bad request" });
+      message = "Bad request";
+      res.status(400).json({ message });
     });
 };
 
 // SAVE
 exports.save = (req, res) => {
-  Category.create(req.body).then(category => {
-    res.status(200).json({ category });
-  });
+  let message = "";
+  Category.create(req.body)
+    .then(data => {
+      message = "Success";
+      res.status(200).json({ message, data });
+    })
+    .catch(error => {
+      message = "Server response error";
+      res.status(500).json({ message });
+    });
 };
 
 // UPDATE
 exports.update = (req, res) => {
+  let message = "";
+
+  const { id } = req.params;
+
   Category.update(req.body, {
-    where: { id: req.params.id }
-  }).then(category => {
-    res.status(200).json({ category });
-  });
+    where: { id }
+  })
+    .then(data => {
+      message = "Success";
+      res.status(200).json({ message });
+    })
+    .catch(error => {
+      message = "Server response error";
+      res.status(500).json({ message });
+    });
 };
 
 // DELETE
 exports.delete = (req, res) => {
-  Category.destroy({
+  let message = "";
+
+  const { id } = req.params;
+
+  Category.findAll({
     where: {
-      id: req.params.id
+      id
     }
   })
-    .then(category => {
-      res.status(200).json({ category });
+    .then(data => {
+      if (!data.length) {
+        message = "No data";
+        res.status(200).json({ message });
+      } else {
+        Category.destroy({
+          where: {
+            id
+          }
+        })
+          .then(category => {
+            message = "Success";
+            res.status(200).json({ message });
+          })
+          .catch(error => {
+            message = "Bad request";
+            res.status(400).json({ message });
+          });
+      }
     })
     .catch(error => {
-      res.status(400).json({ message: "Bad request" });
+      res.status(500).json({
+        massage: "Server response error!"
+      });
     });
 };
